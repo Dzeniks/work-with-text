6875169996489006c923a087e7fd92d9bb0e9c29
Dzeniks <jan.soukenik@hotmail.com>
2024-10-14 13:16:13 +0200

    Seach in numbers

--- tnine.c
+++ tnine.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 struct Phonebook {
     char *names;
@@ -35,40 +36,34 @@ char *get_phone(struct Phonebook phonebook, int idx) {
 
 void print_phonebook(struct Phonebook phonebook) {
     for (int i = 0; i < *phonebook.size; ++i) {
-        printf("%d %s - %s\n", i, get_name(phonebook, i), get_phone(phonebook, i));
+        printf("%s\n%s\n", get_name(phonebook, i), get_phone(phonebook, i));
     }
 }
 
-void extract_phonebook_from_stdin(struct Phonebook phonebook) {
+bool extract_phonebook_from_stdin(struct Phonebook phonebook) {
+    // Extract output from stdin and return if raise error or end successfully
     // Buffers
-    char buffer[phonebook.max_row_size];
+    char line_buffer[phonebook.max_row_size];
     char name_buffer[phonebook.max_row_size];
     char phone_buffer[phonebook.max_row_size];
-
-    const int max_row_size = phonebook.max_row_size;
-    const int max_rows = phonebook.max_rows;
 
     int buffer_size = 0;
     // Start with first line
     int line_count = 1;
     while (true) {
-        // Raise error
-        if (max_row_size <= buffer_size) {
-            fprintf(stderr, "Array index out of range");
-            return;
-        }
-        // Raise error
-        if (max_rows < *phonebook.size) {
-            fprintf(stderr, "Array index out of range");
-            return;
+        // Get char and check if it ends line or file
+        if (phonebook.max_rows < *phonebook.size) {
+            fprintf(stderr, "To much contact on input");
+            return true;
         }
         char c = getchar();
         if (c == '\n' || c == EOF) {
-            buffer[buffer_size] = '\0';
+            // If buffer ends line complete chars with '\0' to make string and copy it to right buffer
+            line_buffer[buffer_size] = '\0';
             if (line_count % 2 == 1) {
-                strcpy(name_buffer, buffer);
+                strcpy(name_buffer, line_buffer);
             } else {
-                strcpy(phone_buffer, buffer);
+                strcpy(phone_buffer, line_buffer);
                 add_contact(phonebook, name_buffer, phone_buffer);
                 strcpy(name_buffer, "");
                 strcpy(phone_buffer, "");
@@ -76,22 +71,66 @@ void extract_phonebook_from_stdin(struct Phonebook phonebook) {
             line_count++;
 
             if (c == EOF) {
-                return;
+                return false;
             }
 
-            strcpy(buffer, "");
+            strcpy(line_buffer, "");
             buffer_size = 0;
             continue;
         }
-        buffer[buffer_size] = c;
+        // Add character to line buffer
+        line_buffer[buffer_size] = c;
         buffer_size++;
     }
 }
 
-int main(int argc, char *argv[]) {
-    printf("Hello world!\n");
 
-    int MAX_ROWS = 42;
+void search_numbers(const struct Phonebook phonebook, const char *searched_num, int find_match[]) {
+    if (searched_num == NULL) {
+        for (int i = 0; i < *phonebook.size; ++i) {
+            find_match[i] = 1;
+        }
+        return;
+    }
+    for (int i = 0; i < *phonebook.size; ++i) {
+        char *phone_number = get_phone(phonebook, i);
+        // find first number from phone_number
+        char *ptr = phone_number;
+        char first_char = searched_num[0];
+        int MATCH = 0;
+        while ((ptr = strchr(ptr, first_char)) != NULL) {
+            // Check if next character match
+            for (int idx = 1; idx < (int) (strlen(searched_num)); ++idx) {
+                // If match next another
+                if (*(ptr + idx) == searched_num[idx]) {
+                    MATCH = 1;
+                } else {
+                    MATCH = 0;
+                    break;
+                }
+            }
+            if (MATCH == 1) {
+                break;
+            }
+            ptr++; // Move the pointer to search for the next occurrence
+        }
+        find_match[i] = MATCH;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // TODO: Remove clock
+    clock_t start = clock();
+
+    // Error flag
+    bool raise_error;
+
+    char *entered_number = NULL;
+    if (1 < argc) {
+        entered_number = argv[1];
+    }
+
+    int MAX_ROWS = 40000;
     int MAX_ROW_SIZE = 100;
 
     char names[MAX_ROWS * MAX_ROW_SIZE];
@@ -100,9 +139,29 @@ int main(int argc, char *argv[]) {
 
     struct Phonebook phonebook = {names, phones, &size, MAX_ROWS, MAX_ROW_SIZE};
 
-    extract_phonebook_from_stdin(phonebook);
+    raise_error = extract_phonebook_from_stdin(phonebook);
+    if (raise_error) { return -1; }
 
+    int find_match[MAX_ROWS];
+    // Go through phonebook numbers
+    search_numbers(phonebook, entered_number, find_match);
+
+    for (int i = 0; i < *phonebook.size; ++i) {
+        if (find_match[i]) {
+            printf("%s - %s\n", get_name(phonebook, i), get_phone(phonebook, i));
+        }
+    }
+
+    printf("\n%d\n", *phonebook.size);
+    // Search in names in order without break
+
+    // Search in names in order with break
 
     printf("End!\n");
+    // TODO: Remove clock
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Time taken: %f seconds\n", cpu_time_used);
     return 0;
 }
