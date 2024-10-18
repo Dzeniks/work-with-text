2c15c73004d09b462e59e89ba11c28b229310ed9
Dzeniks <jan.soukenik@hotmail.com>
2024-10-18 15:05:22 +0200

    opimized version

--- tnine.c
+++ tnine.c
@@ -2,41 +2,61 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_CONTACTS 30000
+#define MAX_CONTACTS 27000
 #define MAX_ROW_SIZE 101
 #define MAX_ENCODE_SIZE 6
 
-void decode_number(char num, char *decoded_chars) {
-    switch (num) {
-        case '2':
-            strcpy(decoded_chars, "abc2");
-        break;
-        case '3':
-            strcpy(decoded_chars, "def3");
-        break;
-        case '4':
-            strcpy(decoded_chars, "ghi4");
-        break;
-        case '5':
-            strcpy(decoded_chars, "jkl5");
-        break;
-        case '6':
-            strcpy(decoded_chars, "mno6");
-        break;
-        case '7':
-            strcpy(decoded_chars, "pqrs7");
-        break;
-        case '8':
-            strcpy(decoded_chars, "tuv8");
-        break;
-        case '9':
-            strcpy(decoded_chars, "wxyz9");
-        break;
-        case '0':
-            strcpy(decoded_chars, "+9");
-        break;
-        default: ;
+
+char encode_char(char c) {
+    switch (c) {
+        case 'a':
+        case 'b':
+        case 'c':
+            return '2';
+        case 'd':
+        case 'e':
+        case 'f':
+            return '3';
+        case 'g':
+        case 'h':
+        case 'i':
+            return '4';
+        case 'j':
+        case 'k':
+        case 'l':
+            return '5';
+        case 'm':
+        case 'n':
+        case 'o':
+            return '6';
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+            return '7';
+        case 't':
+        case 'u':
+        case 'v':
+            return '8';
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            return '9';
+        case '+':
+            return '0';
+        case ' ':
+            return ' ';
+        default:
+            return c;
     }
+}
+
+char *encode_str(char *str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        str[i] = encode_char(str[i]);
+    }
+    return str;
 }
 
 char to_lower(char character) {
@@ -57,7 +77,7 @@ struct Phonebook {
 };
 
 int add_contact(struct Phonebook phonebook, char name[], char number[]) {
-    // Check data validity, add data and return was raised error
+    // Check data validity, add data
     if (strlen(name) <= 0) {
         fprintf(stderr, "Missing name for contact\n");
         return 1;
@@ -95,24 +115,40 @@ char *get_phone(struct Phonebook phonebook, int idx) {
     return &phonebook.phones[idx * MAX_ROW_SIZE];
 }
 
+void name_to_nums(struct Phonebook phonebook, int idx, char *str_out) {
+    char *name = get_name(phonebook,idx);
+    strcpy(str_out, name);
+    encode_str(str_out);
+}
+
 void print_phonebook(struct Phonebook phonebook) {
     for (int pb_idx = 0; pb_idx < *phonebook.size; ++pb_idx) {
         printf("%s, %s\n", get_name(phonebook, pb_idx), get_phone(phonebook, pb_idx));
     }
 }
 
+int search_in_str(const char *str, const char *searched_str) {
+    if (searched_str == NULL) {
+        return 1;
+    }
+    char *ptr = (char *)str;
+    while ((ptr = strstr(ptr, searched_str)) != NULL) {
+        if (ptr != NULL) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int extract_phonebook_from_stdin(struct Phonebook phonebook) {
     // Extract output from stdin and return if raise error or end successfully
-    // Buffers
     char line_buffer[MAX_ROW_SIZE];
     char name_buffer[MAX_ROW_SIZE];
     char phone_buffer[MAX_ROW_SIZE];
     int buffer_size = 0;
 
-    // Start with first line
     int line_count = 1;
     while (1) {
-        // Get char and check if it ends line or file
         if (MAX_CONTACTS <= *phonebook.size) {
             fprintf(stderr, "To much contact on input\n");
             return 1;
@@ -122,10 +158,8 @@ int extract_phonebook_from_stdin(struct Phonebook phonebook) {
             return 1;
         }
         char stdin_char = getchar();
-        // lower char
         stdin_char = to_lower(stdin_char);
         if (stdin_char == '\n' || stdin_char == EOF) {
-            // If buffer ends line complete chars with '\0' to make string and copy it to right buffer
             line_buffer[buffer_size] = '\0';
             if (line_count % 2 == 1) {
                 strcpy(name_buffer, line_buffer);
@@ -155,97 +189,28 @@ int extract_phonebook_from_stdin(struct Phonebook phonebook) {
     }
 }
 
-void search_numbers(const struct Phonebook phonebook, const char *searched_nums, int find_match[]) {
-    if (searched_nums == NULL) {
-        for (int pb_idx = 0; pb_idx < *phonebook.size; ++pb_idx) {
-            find_match[pb_idx] = 1;
-        }
-        return;
+int search_str_array(struct Phonebook phonebook, char *str, const char *searched_str, int *find_match, void (cpy_str)(struct Phonebook, int, char*)) {
+    // Return match size and fill matches in find_match
+    int match_count = 0;
+    for (int entry_idx = 0; entry_idx < *phonebook.size; ++entry_idx) {
+        cpy_str(phonebook, entry_idx, str);
+        int found = search_in_str(str, searched_str);
+        find_match[entry_idx] = found;
+        match_count += found;
     }
-    for (int pb_idx = 0; pb_idx < *phonebook.size; ++pb_idx) {
-        char *phone_number = get_phone(phonebook, pb_idx);
-        char *ptr = phone_number;
-        int found_match = 0;
-        while ((ptr = strstr(ptr, searched_nums)) != NULL) {
-            if (ptr != NULL) {
-                found_match = 1;
-                break;
-            }
-        }
-        find_match[pb_idx] = found_match;
-    }
+    return match_count;
 }
 
-void combination_of_strings(char *strings, int expected_size, int allocated_size, int index, char *current_combination,
-                            char *results, int *result_size) {
-    if (index == expected_size) {
-        strcpy(&results[*result_size * allocated_size], current_combination);
-        *result_size += 1;
-        return;
-    }
-    char *current_string = &strings[index * allocated_size];
-
-    // Copy string
-    char current_combination_copy[allocated_size];
-    strcpy(current_combination_copy, current_combination);
-
-    // Get chars from strings[index] and create combinations
-    char char_for_new_combination;
-    for (int str_idx = 0; (char_for_new_combination = current_string[str_idx]) != '\0'; ++str_idx) {
-        char str_for_new_combination[] = {char_for_new_combination, '\0'};
-        char new_combination[(allocated_size)];
-        int new_index = (index + 1);
-
-        strcpy(new_combination, current_combination);
-        strcat(new_combination, str_for_new_combination);
-        combination_of_strings(strings, expected_size, allocated_size, new_index, new_combination, results,
-                               result_size);
-    }
+int search_numbers(const struct Phonebook phonebook, const char *searched_nums, int find_match_numbers[]) {
+    char temp_num[MAX_ROW_SIZE];
+    return search_str_array(phonebook, temp_num, searched_nums, find_match_numbers, get_phone);
 }
 
-void search_names(struct Phonebook phonebook, char *entered_number, int *find_match_names) {
-    if (entered_number == NULL) {
-        for (int pb_idx = 0; pb_idx < MAX_CONTACTS; ++pb_idx) {
-            find_match_names[pb_idx] = 1;
-        }
-        return;
-    }
-    char entered_num;
-    char all_decoded[MAX_ENCODE_SIZE * strlen(entered_number)];
-    int num_possible_combinations = 1;
-    for (int pb_idx = 0; (entered_num = entered_number[pb_idx]) != '\0'; ++pb_idx) {
-        char decoded_num[MAX_ENCODE_SIZE];
-        decode_number(entered_num, decoded_num);
-        num_possible_combinations *= (int)strlen(decoded_num);
-        strcpy(&all_decoded[MAX_ENCODE_SIZE * pb_idx], decoded_num);
-    }
-    // Generate all combinations
-    char current_combination_test[MAX_ENCODE_SIZE] = "";
-    char combinations[MAX_ENCODE_SIZE * num_possible_combinations];
-    int combinations_size = 0;
-    int expected_combination_size = strlen(entered_number);
-    combination_of_strings(all_decoded, expected_combination_size, MAX_ENCODE_SIZE, 0, current_combination_test,
-                           combinations,
-                           &combinations_size);
-
-    // Search combination in string
-    for (int combination_idx = 0; combination_idx < combinations_size; ++combination_idx) {
-        for (int names_idx = 0; names_idx < *phonebook.size; ++names_idx) {
-            char *phone_number = get_name(phonebook, names_idx);
-            int found_match = 0;
-            if (find_match_names[names_idx] == 1) {
-                break;
-            }
-            while ((phone_number = strstr(phone_number, &combinations[MAX_ENCODE_SIZE * combination_idx])) != NULL) {
-                if (phone_number != NULL) {
-                    found_match = 1;
-                    break;
-                }
-            }
-            find_match_names[names_idx] = found_match;
-        }
-    }
+int search_names(struct Phonebook phonebook, char *searched_nums, int *find_match_names) {
+    char temp_name[MAX_ROW_SIZE];
+    return search_str_array(phonebook, temp_name, searched_nums, find_match_names, name_to_nums);
 }
+
 
 int main(int argc, char *argv[]) {
     // Error flag
@@ -267,11 +232,16 @@ int main(int argc, char *argv[]) {
 
     // Go through phonebook numbers
     int find_match_numbers[MAX_CONTACTS];
-    search_numbers(phonebook, entered_search_number, find_match_numbers);
+    int numbers_match_count = search_numbers(phonebook, entered_search_number, find_match_numbers);
 
     // Go through phonebook names
     int find_match_names[MAX_CONTACTS];
-    search_names(phonebook, entered_search_number, find_match_names);
+    int names_match_count = search_names(phonebook, entered_search_number, find_match_names);
+
+    if ((names_match_count + numbers_match_count) == 0) {
+        printf("Not found\n");
+        return 0;
+    }
 
     // Union of matches
     int matches[MAX_CONTACTS];
@@ -281,7 +251,7 @@ int main(int argc, char *argv[]) {
 
     for (int pb_idx = 0; pb_idx < *phonebook.size; ++pb_idx) {
         if (matches[pb_idx] == 1) {
-            printf("%s, %s\n", get_name(phonebook, pb_idx), get_phone(phonebook, pb_idx));
+        printf("%s, %s\n", get_name(phonebook, pb_idx), get_phone(phonebook, pb_idx));
         }
     }
     return 0;
